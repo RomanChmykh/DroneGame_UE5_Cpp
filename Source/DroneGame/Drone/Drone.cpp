63f453a7d7310b8086c8922e588a67ce0c903189
Roman <romanchmykh20@gmail.com>
2023-10-22 19:28:37 +0300

    Created cpp pawn class for drone. Inherited blueprint class from cpp class. Created components in cpp class pawn and setup input for movement.

---
+++ Source/DroneGame/Drone/Drone.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Drone.h"
+#include "Components/BoxComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+
+// Sets default values
+ADrone::ADrone()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>("BoxComponent");
+	RootComponent = BoxComponent;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+	CameraComponent->SetupAttachment(GetRootComponent());
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
+	StaticMeshComponent->SetupAttachment(GetRootComponent());
+
+	FloatingPawnMovement = CreateDefaultSubobject<UFloatingPawnMovement>("FloatingPawnMovement");
+}
+
+// Called when the game starts or when spawned
+void ADrone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADrone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ADrone::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForwardBack", this, &ADrone::MoveForwardBack);
+	PlayerInputComponent->BindAxis("MoveRightLeft", this, &ADrone::MoveRightLeft);
+	PlayerInputComponent->BindAxis("MoveUp", this, &ADrone::MoveUp);
+	PlayerInputComponent->BindAxis("MoveDown", this, &ADrone::MoveDown);
+	
+}
+
+void ADrone::MoveForwardBack(float Amount)
+{
+	AddMovementInput(GetActorForwardVector(), Amount);
+}
+
+void ADrone::MoveRightLeft(float Amount)
+{
+	AddMovementInput(GetActorRightVector(), Amount);
+}
+
+void ADrone::MoveUp(float Amount)
+{
+	AddMovementInput(GetActorUpVector(), Amount);
+}
+
+void ADrone::MoveDown(float Amount)
+{
+	AddMovementInput(GetActorUpVector(), Amount);
+}
+
---
+++ Source/DroneGame/Drone/Drone.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Drone.generated.h"
+
+class UBoxComponent;
+class UCameraComponent;
+class UStaticMeshComponent;
+class UFloatingPawnMovement;
+
+UCLASS()
+class DRONEGAME_API ADrone : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	
+	ADrone();
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UBoxComponent* BoxComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UFloatingPawnMovement* FloatingPawnMovement;
+
+public:	
+	
+	virtual void Tick(float DeltaTime) override;
+
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+private:
+	void MoveForwardBack(float Amount);
+	void MoveRightLeft(float Amount);
+	void MoveUp(float Amount);
+	void MoveDown(float Amount);
+};
